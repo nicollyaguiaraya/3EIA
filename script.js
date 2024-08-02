1b6274d57a65ee1624bde4e0fb3262f94c51a385
nicollyaguiaraya <146956921+nicollyaguiaraya@users.noreply.github.com>
2024-08-02 09:28:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaprincipal = document.querySelector('.caixa-principal');
+const caixapergunta = document.querySelector('.caixa-pergunta');
+const caixaalternativa = document.querySelector('.caixa-alternativa');
+const caixaresultado = document.querySelector('.caixa-resultado');
+
+const pergunta=[//abre lista de objetos (itens)
+    {// abre o item
+enunciado:"pergunta1"
+alternativas:[
+    "alternativa1"
+    "alternativa2"
+]
+    )
+]
+
